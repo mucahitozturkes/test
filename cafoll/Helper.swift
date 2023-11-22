5da5959345dc317e53001115f47611f9472c2304
mucahitozturkes <138803167+mucahitozturkes@users.noreply.github.com>
2023-11-22 22:42:11 +0300

    Update Helper.swift

--- cafoll/Helper.swift
+++ cafoll/Helper.swift
@@ -11,5 +11,16 @@ class Helper {
     
     var context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     var filePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-    
+    var foods: [Foods]?
+    //Fetch Foods
+    func fetchFoods() {
+        do {
+        let request = Foods.fetchrequest()
+        self.foods = try context.fetch(request)
+        } 
+        catch {
+        print(error)
+        }
+    }
 }
+

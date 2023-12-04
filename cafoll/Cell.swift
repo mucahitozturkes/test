c6d3d2f304566647988d330eea8db71532e99624
mucahitozturkes <138803167+mucahitozturkes@users.noreply.github.com>
2023-12-04 15:06:06 +0300

    Add files via upload
    cell added

---
+++ cafoll/Cell.swift
@@ -0,0 +1,16 @@
+//
+//  Cell.swift
+//  cafoll
+//
+//  Created by mücahit öztürk on 22.11.2023.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+    
+    @IBOutlet weak var favoriteIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var favoriteButton: UIButton!
+    @IBOutlet weak var moreInfoButton: UIButton!
+    @IBOutlet weak var foodTitleLabel: UILabel!
+}
